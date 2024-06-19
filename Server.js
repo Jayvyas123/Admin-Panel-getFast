811b13f156cf58670428b979c70eceabb9a7a73a
unknown <jaykumarvyas1@gmail.com>
2024-06-19 12:42:27 +0530

    Project Setup

---
+++ Server.js
@@ -0,0 +1,13 @@
+// // const http = require('http')
+// // http.createServer((req,resp)=>{
+// //     resp.write("This is An Exmple File..!")
+// //     resp.end()
+
+   
+// // }).listen(8080);
+
+// const fs = require('fs')
+// fs.appendFile(__dirname,"Apple.html","Backend")
+
+const os = require('os');
+console.log(os.userInfo())
